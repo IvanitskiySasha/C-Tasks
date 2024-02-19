91afdb4182782543a1a053287671417f12883671
Aleksandr <ivanitskiysasha@mail.ru>
2024-02-19 18:59:49 +0300

    Создал 3 задачи из 1го семинара

--- Program.cs
+++ Program.cs
@@ -35,91 +35,90 @@
 // else Console.WriteLine("Квадрат числа {1} НЕ равен {0}", number1, number2);
 
 
-//Задача1
-// Напишите программу, которая на вход принимает два целых числа и проверяет,
-// является ли первое число квадратом второго.
-
-// Console.Write("Введите первое число: ");
-// int number1 = Convert.ToInt32(Console.ReadLine());
-// // int.Parse("12345");
-// Console.Write("Введите второе число: ");
-// int number2 = int.Parse(Console.ReadLine());
-
-// if(number1 == number2 * number2)
-// {
-//     Console.WriteLine("Да");
-// }
-// else
-// {
-//     Console.WriteLine("Нет");
-// }
 
 
-// if(number1 == Math.Pow(number2,2))
-// {
-//     Console.WriteLine($"Квадрат числа {number2} равен {number1}");
-// }
-// else
-// {
-//     Console.WriteLine("Квадрат числа числа {1} НЕ равен {0}", number1, number2);
-// }
 
-// // Console.WriteLine("Значение переменной number1 равно",number1,трям);
 
-// Задача 2
-// Напишите программу, которая на вход принимает целое число N, а на выходе
-// показывает все целые числа в промежутке от -N до N.
 
-// Console.Write("Введите число N: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-
-// if (num < 0)
-// {
-//   num = num*(- 1);
-// }
-// int index = - num;
-// while (index <= num)
-// {
-//   Console.Write($"{index} ");
-//   index++;
-// }
-
-// Задача 3
-// Напишите программу, которая принимает на вход трёхзначное целое число
-// и на выходе показывает сумму первой и последней цифры этого числа.
 
 // Console.Write("Введите трехзначное число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
+// string num = Console.ReadLine()!;
+// Console.WriteLine(Convert.ToInt32(Convert.ToString(num[0])) +
+//                     Convert.ToInt32(Convert.ToString(num[2])));
 
-// while (true)
+
+// Console.Write("firstNumber: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("secondNumber: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
 // {
-//     if ((num <= 999) && (num >= 100)) break;
-//     Console.WriteLine("Введено НЕ трехзначное число: ");
-//     Console.Write("Введите трехзначное число: ");
-//     num = Convert.ToInt32(Console.ReadLine());
-
+//     Console.Write("первое число больше");
+// }
+// else
+// {
+//     Console.Write("второе число больше");
 // }
 
-// int num1 = num % 10;
-// int num2 = num / 100;
-
-// Console.WriteLine($"Сумма {num1} и {num2} равна {num1 + num2}");
 
 
-// int num3 = num / 10 % 10;
-// Console.WriteLine(num3);
+// Console.Write("firstNumber: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("secondNumber: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2)
+// {
+//     Console.Write("первое число больше");
+// }
+// if (num1 < num2)
+// {
+//     Console.Write("второе число больше");
+// }
+// if (num1 == num2)
+// {
+//     Console.Write("числа равны");
+// }
 
-// int num = new Random().Next(100, 1000);
 
-// int num1 = num % 10;
-// int num2 = num / 100;
+// using System;
 
-// Console.WriteLine($"Число {num}. Сумма {num1} и {num2} равна {num1 + num2}");
+// public class Answer {
+//     static void CompareNumbers(int firstNumber, int secondNumber)
+//     {
+//         // Введите свое решение ниже
+//       Console.Write("firstNumber: ");
+//       int num1 = Convert.ToInt32(Console.ReadLine());
+//       Console.Write("secondNumber: ");
+//       int num2 = Convert.ToInt32(Console.ReadLine());
+//       if (num1 > num2)
+//       {
+//          Console.Write("первое число больше");
+//       }
+//       if (num1 < num2)
+//       {
+//          Console.Write("второе число больше");
+//       }
+//       if (num1 == num2)
+//       {
+//          Console.Write("числа равны");
+//       }
+//     }
 
-// 123 % 10 -> 3
-// 123 % 100 -> 23
 
-Console.Write("Введите трехзначное число: ");
-string num = Console.ReadLine()!;
-Console.WriteLine(Convert.ToInt32(Convert.ToString(num[0])) +
-                    Convert.ToInt32(Convert.ToString(num[2])));
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int firstNumber, secondNumber;
+
+//         if (args.Length >= 2) {
+//             firstNumber = int.Parse(args[0]);
+//             secondNumber = int.Parse(args[1]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             firstNumber = 5;
+//             secondNumber = 6;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CompareNumbers(firstNumber, secondNumber);
+//     }
+// }
